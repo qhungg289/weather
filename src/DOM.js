1553b5e013356254aa3ed7200df58f1c5ed8fd5f
Hung Duong Quang <qhungg289@protonmail.com>
2021-08-13 21:16:42 +0700

    Update DOM.js

--- src/DOM.js
+++ src/DOM.js
@@ -1,20 +1,27 @@
 const form = document.querySelector("form");
 const input = document.getElementById("location-input");
+const unitSwitch = document.getElementById("unit-switch");
 
 function renderPage(weatherData) {
 	const locationDisplay = document.getElementById("location");
 	const tempDisplay = document.getElementById("temp");
+	const tempHighDisplay = document.getElementById("temp-high");
+	const tempLowDisplay = document.getElementById("temp-low");
 	const feelsLikeDisplay = document.getElementById("feels-like");
 	const humidityDisplay = document.getElementById("humidity");
+	const rainInHourDisplay = document.getElementById("rain-in-hour");
 	const weatherMainDisplay = document.getElementById("weather-main");
 	const weatherDescDisplay = document.getElementById("weather-desc");
 
 	locationDisplay.innerHTML = weatherData.location;
 	tempDisplay.innerHTML = `${weatherData.temp}&deg;`;
+	tempHighDisplay.innerHTML = `${weatherData.tempHigh}&deg;`;
+	tempLowDisplay.innerHTML = `${weatherData.tempLow}&deg;`;
 	feelsLikeDisplay.innerHTML = `${weatherData.feelsLike}&deg;`;
 	humidityDisplay.innerHTML = `${weatherData.humidity}%`;
+	rainInHourDisplay.innerHTML = `${weatherData.rainInHour}mm`;
 	weatherMainDisplay.innerHTML = weatherData.weatherMain;
 	weatherDescDisplay.innerHTML = weatherData.weatherDesc;
 }
 
-export { form, input, renderPage };
+export { form, input, unitSwitch, renderPage };
